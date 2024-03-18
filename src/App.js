bcfbc4dfc809afc64394b84ac86ed36bdebdb155
Kratos <115424812+KratosXVII@users.noreply.github.com>
2024-03-18 15:13:50 +0100

    Added the footer

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Analytics from "./components/Analytics";
 import Newsletter from "./components/Newsletter";
 import Features from "./components/Features";
 import Contact from "./components/Contact";
+import Footer from "./components/Footer";
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
       <Newsletter/>
       <Features/>
       <Contact/>
+      <Footer/>
     </div>
   );
 }
